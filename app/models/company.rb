08498538aca943851652f08a76d3df39bd911e4b
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-04-29 12:12:07 +0900

    アソシエーション・バリデーションの設定

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,10 @@
 class Company < ApplicationRecord
+  # アソシエーションの設定
+  belong_to :user
+
+  # 値が空になるのを防ぐ
+  with_options presence: true do
+    validates :name
+    validates :address
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  # アソシエーションの設定
+  has_one :company
   # パスワードをハッシュ化
-  has_secure_password
+  
+  # 値が空になるのを防ぐ
+  with_options presence: true do
+    validates :name
+    validates :email
+    validates :password_digest
+  end
 end
